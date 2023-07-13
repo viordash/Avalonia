cf5ef44529d0ef148d558efbff017ef2394fff93
Vitaliy Orazov <viordash@mail.ru>
2023-07-14 01:14:31 +0300

    wip. add linux tests project

---
+++ Avalonia.Linux.NUnit.UnitTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Avalonia.Linux.NUnit.UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
